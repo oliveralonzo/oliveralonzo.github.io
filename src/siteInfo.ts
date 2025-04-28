b3256236d0d91dfbe14675486d5eef5ef834cac1
oliveralonzo <oliveralonzof@gmail.com>
2025-04-29 06:21:22 +0900

    Update last updated date

--- src/siteInfo.ts
+++ src/siteInfo.ts
@@ -1,5 +1,5 @@
 export const siteInfo = {
-    lastUpdated: "2025-04-22",
+    lastUpdated: "2025-04-29",
     firstName: "Oliver",
     lastName: "Alonzo",
     description: "Assistant Professor at DePaul University"
